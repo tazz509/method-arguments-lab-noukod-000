fd2fe6c455a1e25ac780a42518092856f1eac34f
carly <carly@noukod.com>
2018-09-13 15:05:36 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction (name)
 puts  "using the #{name}"
 end
 
-def introduction (name,language)
+def introduction_with_language (name,language)
   puts "using the #{name} to explain a #{language}"
 end
